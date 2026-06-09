e8c3bcda33fafb941aafd54e3a8ca4c6d0a0dd93
Thierry Garcez <thpc1964@gmail.com>
2026-06-09 16:17:59 -0300

    Update simular_sensor.py

--- simular_sensor.py
+++ simular_sensor.py
@@ -6,7 +6,7 @@ while True:
 
     temperatura = round(random.uniform(-10, 3), 1)
     umidade = random.randint(60, 80)
-    bateria = random.randint(80, 100)
+    bateria = random.randint(85, 100)
     vazamento = random.choice([0, 0, 0, 0, 0, 0, 1])
 
     conn = sqlite3.connect("banco.db")
